53a1e2024bb7de86abbcf449b502d9fb557ba698
Aditya Ozalwar <adityaozalwarcnp@gmail.com>
2024-07-13 17:13:02 +0530

    Database setup

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+dotenv.config({path:'./backend/config.env'})
+const app = express();
+
+const connectDb = () =>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log(`Connected to DB!!`)
+    }).catch((err)=>{
+        err
+    })
+}
+
+app.listen(process.env.PORT,()=>{
+    connectDb();
+    console.log(`Server is working : ${process.env.PORT}`)
+})
