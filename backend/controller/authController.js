e6bae777290b1b8fb69dcb8fca5a4492319b433b
Aditya Ozalwar <adityaozalwarcnp@gmail.com>
2024-07-13 17:56:39 +0530

    Middleware & ErrorHandler

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,14 +1,15 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
+import { errorHandler } from "../utils/error.js"
 
-export const register = async (req,res) => {
+export const register = async (req,res,next) => {
     const {username,email,password} = req.body
     try {
         const hashedPassword = bcrypt.hashSync(password,10)
         const newUser = User({username,email,password:hashedPassword})
         await newUser.save()
         return res.status(201).json({message:"User is registered successfully!!!"})
-    } catch (error) {
-        return res.status(404).json({message:error.message})
+    } catch (error) {        
+        next(errorHandler(500, "Something went wrong!"));
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -22,3 +22,13 @@ app.listen(process.env.PORT,()=>{
 app.use(express.json())
 
 app.use("/api/auth",authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+})
